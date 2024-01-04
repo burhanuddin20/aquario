d58a9db2b92e4e57c2cbea5f42e277f277cd591a
Burhan Uddin <55764672+burhanuddin20@users.noreply.github.com>
2024-01-04 13:30:32 +0000

    bug fixes

--- aquario/Assets/Scripts/MovementScript.cs
+++ aquario/Assets/Scripts/MovementScript.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,10 +6,25 @@ using UnityEngine;
 public class MovementScript : MonoBehaviour
 {
     public Camera cam;
-    public float speed = 1f;
+    public float speed = 5f;
 
     public FloatingJoystick joystick;
 
+    public float minX;
+    public float maxX;
+    public float minY;
+    public float maxY;
+
+    public SmoothCameraFollow cameraFollowScript;
+
+    private void Start()
+    {
+        // cameraFollowScript = cam.GetComponent<SmoothCameraFollow>();
+        // minX = cameraFollowScript.minX;
+        // maxX = cameraFollowScript.maxX;
+        // maxY = cameraFollowScript.maxY;
+        // minY = cameraFollowScript.minY;
+    }
 
     // Update is called once per frame
     void Update()
@@ -18,6 +34,11 @@ public class MovementScript : MonoBehaviour
 
         // Calculate new position based on joystick input
         Vector3 newPosition = transform.position + inputDirection * (speed * Time.deltaTime);
+
+        
+        // clamp so you cannot go outside world bounds
+        //newPosition.x = Mathf.Clamp(newPosition.x, minX, maxX);
+        //newPosition.y = Mathf.Clamp(newPosition.y, minY, maxY);
 
         // Maintain the original z position
         newPosition.z = transform.position.z;
--- aquario/Assets/Scripts/SmoothCameraFollow.cs
+++ aquario/Assets/Scripts/SmoothCameraFollow.cs
@@ -1,36 +1,55 @@
 using System;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Tilemaps;
 
 public class SmoothCameraFollow : MonoBehaviour 
 {
-
     public Transform target;
     public float speed;
     public float scaleSpeed;
     public Camera cam;
+    public Tilemap tilemap;
+
+    // World bounds
+    public float minX;
+    public float maxX;
+    public float minY;
+    public float maxY;
+
 
     private void Start()
     {
-        // target = PlayerManager.instance.player.transform;
-        
-        // Set the transform information for the 
+        Bounds tilemapBounds = tilemap.localBounds; // Now, get the bounds
+
+        float halfHeight = cam.orthographicSize;
+        float halfWidth = halfHeight * cam.aspect;
+
+        minX = tilemapBounds.min.x + halfWidth;
+        maxX = tilemapBounds.max.x - halfWidth;
+        minY = tilemapBounds.min.y + halfHeight;
+        maxY = tilemapBounds.max.y - halfHeight;
+        Debug.Log("Minx" + minX);
     }
 
-    // Update is called once per frame
+
     void Update()
     {
+        // Move camera towards target
         Vector3 positionLerp = Vector3.Lerp(transform.position, target.position, Time.deltaTime * speed);
         positionLerp.z = transform.position.z;
+
+        // Clamp the camera's position to the world bounds
+        positionLerp.x = Mathf.Clamp(positionLerp.x, minX, maxX);
+        positionLerp.y = Mathf.Clamp(positionLerp.y, minY, maxY);
+
         transform.position = positionLerp;
 
+        // Adjust the camera's size based on the target's scale
         cam.orthographicSize = Mathf.Lerp(cam.orthographicSize, 5 * target.localScale.x, scaleSpeed * Time.deltaTime);
     }
-    
-    public void SetTarget(GameObject target)
+
+    public void SetTarget(GameObject targetGameObject)
     {
-        Debug.Log("Setting target");
-        this.target = target.transform;
+        this.target = targetGameObject.transform;
     }
 }
